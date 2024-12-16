6e5caf110ac58cc24b867ae71b2e030c3b4308c6
prasad-bigdp <bigdp585@gmail.com>
2024-12-16 08:52:35 +0530

    update

---
+++ JS/code/primitveDataTypes/script.js
@@ -0,0 +1,22 @@
+let a = 25.1759;
+console.log(a);
+console.log(typeof a)
+console.log(a.toFixed(2))// fixed no of decimal digits
+console.log(a.toPrecision(3)) // fixed no of digits
+//25.1759==>25.176==>25.18==>25.2;
+a = 25;
+console.log(a.toString(2))
+
+//Math methods
+
+console.log(Math.max(5, 8, 2, 5, 3));
+console.log(Math.min(5, 8, 2, 5, 3));
+console.log(Math.sqrt(16));
+console.log(Math.cbrt(27))
+console.log(Math.ceil(2.4)) //3
+console.log(Math.floor(2.4)) //3
+console.log(Math.round(3.14))
+console.log(Math.random())
+
+
+
