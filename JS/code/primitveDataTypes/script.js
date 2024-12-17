5322b78f4a755ebd03359398d0cdaf5c6a7f57d8
prasad-bigdp <bigdp585@gmail.com>
2024-12-17 08:51:23 +0530

    message

--- JS/code/primitveDataTypes/script.js
+++ JS/code/primitveDataTypes/script.js
@@ -17,6 +17,24 @@ console.log(Math.ceil(2.4)) //3
 console.log(Math.floor(2.4)) //3
 console.log(Math.round(3.14))
 console.log(Math.random())
+console.log(Math.pow(2,3), 2**3) //8
+console.log(Math.floor(Math.random() * 10000))
+
+//strings
+let str = 'raj';
+let str1 = "raj";
+console.log(typeof str, typeof str1);
+let quote = "The Gandhi Said,'Good night'"
+let time = 'The Time is 5 o\' clock';
+let cost = 25, quantity = 4;
+console.log(` The total of ${quantity}  apples of each worth ${cost} is ${cost * quantity} `)
+
+let s = "prasad";
+console.log(s.length) //6
+console.log(s.charAt(1))
+console.log(s.indexOf('a')); //2
+console.log(s.lastIndexOf('a')) //4
+
 
 
 
