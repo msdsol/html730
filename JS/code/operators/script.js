d0f8a9136f3db5552743d04585b07d15f86f092d
prasad-bigdp <bigdp585@gmail.com>
2024-12-20 08:54:57 +0530

    update

---
+++ JS/code/operators/script.js
@@ -0,0 +1,27 @@
+/* arithematic operators */
+let a = 20, b = 10;
+console.log(`The sum of ${a} and ${b} is ${a+b}`);
+console.log(`The differnce between  ${a} and ${b} is ${a-b}`)
+console.log(`The product of ${a} and ${b} is ${a*b}`)
+console.log(`The division of ${a} and ${b} is ${a/b}`)
+console.log(`The remainder of ${a} and ${b} is ${a%b}`)
+console.log(`The powder of ${a} to ${b} is ${a ** b}`)
+
+/* relational operators */
+let a1=20,b1=5,c1='20'
+console.log(a1 > b1); //T
+console.log(a1 < b1); //F
+console.log(a1 >= b1); //T
+console.log(a1 <= b1); //F
+console.log(a1 == c1); //T
+console.log(a1 === c1); //F
+console.log(a1 != c1); //F
+console.log(a1 !== c1); //T
+
+
+
+
+
+
+
+
