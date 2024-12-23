0792d78e16ee6b048ddd0f595133c07caede05d5
prasad-bigdp <bigdp585@gmail.com>
2024-12-23 08:48:04 +0530

    update

--- JS/code/operators/script.js
+++ JS/code/operators/script.js
@@ -18,6 +18,67 @@ console.log(a1 === c1); //F
 console.log(a1 != c1); //F
 console.log(a1 !== c1); //T
 
+/* logical operators*/
+a = 20; b = 10;
+console.log("Logical AND will be true when both statements are true only" + ((a > 2) && (b > 3)))
+console.log("Logical OR will be true when any of the statements are true only" + (a > 2 || b > 30))
+console.log("Logical NOT will be true when statement is false" +!(a > 20 ))
+
+/* assignment or shorthand operators */
+console.log(a += 2);//22
+console.log((a -= 2))//20
+console.log((a *= 2))//40
+console.log((a /= 2))//20
+console.log((a **= 2))//400
+console.log((a %= 2))//0
+
+a = 5;
+b = a++;//a=a+1
+c = ++a;
+console.log(a, b, c) //7 5 7
+
+a = 20, b = 10;
+c = ++a + a++ + --b + b-- //21+21+9+9
+console.log(c) //60
+
+/* ternary operators */
+a = 11;
+c = (a % 2 == 0) ? 'Even' : 'Odd'
+console.log(c)
+
+console.log('5' + 1) //'51'
+console.log(+'5' + 1) //6
+a=5
+var d=25;
+c = d ?? a;
+console.log(c)
+d = undefined;
+a = 10;
+c = d ?? a;
+console.log(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
