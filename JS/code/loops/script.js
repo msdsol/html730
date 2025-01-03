608669a149d698d0d6b00976a9fdb1e2e5ca8439
prasad-bigdp <bigdp585@gmail.com>
2025-01-03 08:46:25 +0530

    update

--- JS/code/loops/script.js
+++ JS/code/loops/script.js
@@ -25,6 +25,100 @@ while (i < str.length)
 
 
 
+// while loop to print even numbers from 1 to 20
+let n = 2;
+while (n <= 20)
+{
+    console.log(n)
+    n+=2;
+}
+
+/* take a name and make acronym of the name */
+/* input: "Durga Prasad" ==>DP, "World Health Organization"==>WHO */
+
+// function makeAcronym (str)
+// {
+//     let i = 0, str1 =str[0];
+//     while (i < str.length)
+//     {
+//         if (str.charAt(i) == '')
+//         {
+//             str1=str1+str.charAt(i+1)
+//         }
+//         i++;
+//     }
+//     return str1.toUpperCase()
+// }
+// let res = makeAcronym("World Health Organization")
+// console.log(res)
+
+ i = 10;
+while (i < 10)
+{
+    console.log(i);
+    i++;
+}
+i = 10;
+do
+{
+    console.log(i);
+    i++;
+} while (i < 10);
+
+
+/* print  20 to 1 even numbers in reverse order, like 20 18 16 14 ....*/
+ i = 20
+while (i > 1) {
+	console.log(i)
+	i -= 2
+}
+n = 20
+do {
+	console.log(n)
+	n -= 2
+} while (n > 0)
+
+for (n = 20; n >= 1; n -= 2)
+{
+    console.log(n)
+}
+
+/* using standard for , print array elements */
+let arr = [5, 6, 7, 8, 9]
+for (i = 0; i < arr.length; i++)
+{
+    console.log(arr[i])
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
